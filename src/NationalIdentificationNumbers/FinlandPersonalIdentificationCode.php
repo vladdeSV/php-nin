4ce2c585c81d8935eac7223bacee74db001faf3b
Vladimirs Nordholm <v@vladde.net>
2020-04-06 13:02:59 +0200

    Update Finland NIN handling

--- src/NationalIdentificationNumbers/FinlandPersonalIdentificationCode.php
+++ src/NationalIdentificationNumbers/FinlandPersonalIdentificationCode.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace NIN\NationalIdentificationNumbers;
 
 use DateTimeImmutable;
-use Exception;
 use InvalidArgumentException;
 
 /**
@@ -26,22 +25,31 @@ class FinlandPersonalIdentificationCode implements NationalIdentificationNumberI
     public const COUNTRY_CODE = 'FI';
 
     private const REGEX_PERSONAL_IDENTITY_CODE = /** @lang PhpRegExp */
-        '/^(\d{6})([-+A])(\d{3})([\dA-Y])$/';
+        '/^'
+        . '(?<DD>\d{2})'
+        . '(?<MM>\d{2})'
+        . '(?<YY>\d{2})'
+        . '(?<separator>[-+A])'
+        . '(?<individualNumber>\d{3})'
+        . '(?<checksum>[\dA-Y])'
+        . '$/';
 
     public function __construct(string $nationalIdentificationNumber)
     {
         $matches = [];
         if (!preg_match(self::REGEX_PERSONAL_IDENTITY_CODE, $nationalIdentificationNumber, $matches)) {
-            throw new InvalidArgumentException('Invalid format. Must follow DDMMYYCXXXX');
+            throw new InvalidArgumentException('Invalid format.');
         }
 
-        $dateString = $matches[1];
-        $separator = $matches[2];
-        $individualNumber = (int)$matches[3];
-        $checksum = $matches[4];
+        $twoDigitYear = (int)$matches['YY'];
+        $month = (int)$matches['MM'];
+        $day = (int)$matches['DD'];
+        $separator = $matches['separator'];
+        $individualNumber = (int)$matches['individualNumber'];
+        $checksum = $matches['checksum'];
         $isTemporary = false;
 
-        if ($individualNumber < 2) {
+        if ($individualNumber <= 1) {
             throw new InvalidArgumentException('Invalid individual number.');
         }
 
@@ -49,25 +57,10 @@ class FinlandPersonalIdentificationCode implements NationalIdentificationNumberI
             $isTemporary = true;
         }
 
-        [$day, $month, $twoDigitYear] = str_split($dateString, 2);
+        $century = $this->getCenturyFromSeparator($separator);
+        $year = $century + $twoDigitYear;
 
-        $century = null;
-        /** @noinspection PhpSwitchCaseWithoutDefaultBranchInspection RegExp ensures separator is either +, -, or A */
-        switch ($separator) {
-            case '+':
-                $century = 18;
-                break;
-            case '-':
-                $century = 19;
-                break;
-            case 'A':
-                $century = 20;
-                break;
-        }
-
-        $year = (int)($century . $twoDigitYear);
-
-        if (!checkdate((int)$month, (int)$day, (int)$year)) {
+        if (!checkdate($month, $day, $year)) {
             throw new InvalidArgumentException("Invalid date. {$year}-{$month}-{$day} does not exist.");
         }
 
@@ -89,8 +82,26 @@ class FinlandPersonalIdentificationCode implements NationalIdentificationNumberI
 
     public function __toString(): string
     {
-        $century = (int)((int)$this->date->format('Y') / 100);
+        $separator = $this->getSeparatorFromDate($this->date);
+        $checksum = self::calculateChecksum($this->date, $this->individualNumber);
+
+        return sprintf("%02d%02d%02d%s%03d%s", (int)$this->date->format('d'), (int)$this->date->format('m'), (int)$this->date->format('y'), $separator, $this->individualNumber, $checksum);
+    }
+
+    private static function calculateChecksum(DateTimeImmutable $date, int $individualNumber)
+    {
+        $numberMap = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'];
+
+        $number = (int)(sprintf('%02d%02d%02d%03d', $date->format('d'), $date->format('m'), $date->format('y'), $individualNumber));
+
+        return $numberMap[round(fmod($number / 31.0, 1) * 31)];
+    }
+
+    private function getSeparatorFromDate(DateTimeImmutable $date): string
+    {
+        $century = (int)((int)$date->format('Y') / 100);
         $separator = null;
+
         /** @noinspection PhpSwitchCaseWithoutDefaultBranchInspection Invalid dates are filtered on creation */
         switch ($century) {
             case 18:
@@ -104,18 +115,28 @@ class FinlandPersonalIdentificationCode implements NationalIdentificationNumberI
                 break;
         }
 
-        $checksum = self::calculateChecksum($this->date, $this->individualNumber);
-
-        return sprintf("%02d%02d%02d%s%03d%s", (int)$this->date->format('d'), (int)$this->date->format('m'), (int)$this->date->format('y'), $separator, $this->individualNumber, $checksum);
+        assert($separator !== null);
+        return $separator;
     }
 
-    private static function calculateChecksum(DateTimeImmutable $date, int $individualNumber)
+    private function getCenturyFromSeparator($separator): int
     {
-        $numberMap = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'];
+        $century = null;
+        /** @noinspection PhpSwitchCaseWithoutDefaultBranchInspection RegExp ensures separator is either +, -, or A */
+        switch ($separator) {
+            case '+':
+                $century = 1800;
+                break;
+            case '-':
+                $century = 1900;
+                break;
+            case 'A':
+                $century = 2000;
+                break;
+        }
 
-        $number = (int)(sprintf('%02d%02d%02d%03d', $date->format('d'), $date->format('m'), $date->format('y'), $individualNumber));
-
-        return $numberMap[round(fmod($number / 31.0, 1) * 31)];
+        assert($century !== null);
+        return $century;
     }
 
     /**
