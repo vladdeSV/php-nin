4795d95c631762de66408afd31cd73cab5836030
Vladimirs Nordholm <v@vladde.net>
2020-04-10 16:31:29 +0200

    Add tests for country code helper

---
+++ tests/Helpers/CountryCodeHelperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace NIN\Tests\Helpers;
+
+use NIN\Helpers\CountryCodeHelper;
+use PHPUnit\Framework\TestCase;
+
+class CountryCodeHelperTest extends TestCase
+{
+    /**
+     * @dataProvider validCountryCodes
+     * @param string $countryCode
+     */
+    public function testValidCountryCodes(string $countryCode)
+    {
+        self::assertTrue(CountryCodeHelper::isValidCountryCode($countryCode));
+    }
+
+    /**
+     * @dataProvider invalidCountryCodes
+     * @param string $countryCode
+     */
+    public function testInvalidCountryCodes(string $countryCode)
+    {
+        self::assertFalse(CountryCodeHelper::isValidCountryCode($countryCode));
+    }
+
+    public function validCountryCodes()
+    {
+        return [
+            ['SE'],
+            ['FI'],
+            ['NO'],
+            ['PL'],
+            ['GB'],
+        ];
+    }
+
+    public function invalidCountryCodes()
+    {
+        return [
+            ['se'],
+            ['xx'],
+            ['-'],
+            ['P'],
+            ['PLL'],
+            [''],
+            ['XX'],
+        ];
+    }
+}
